adeb1f0a0774cc647e2646a0f495cdac0a2aa019
mangina <mangina.yulya@gmail.com>
2015-10-17 03:53:13 +0500

    Исправления замечаний

--- phoneBook.js
+++ phoneBook.js
@@ -1,19 +1,20 @@
 'use strict';
 
 var phoneBook = [];// Здесь вы храните записи как хотите
-var person = {};
 /*
    Функция добавления записи в телефонную книгу.
    На вход может прийти что угодно, будьте осторожны.
 */
 module.exports.add = function add(name, phone, email) {
-	
+    var person = {};
     function checkPhone(phone) {
-        return /^((\+)?(\d{1,3})[\- ]?)?(\(\d{3}\)[\- ]?|(\d{3})[\- ]?)[\d\- ]{7,10}/.test(phone);
+        var regPhone = /^((\+)?(\d{1,3})[\- ]?)?(\(\d{3}\)[\- ]?|(\d{3})[\- ]?)[\d\- ]{7,10}/;
+        return regPhone.test(phone);
     }
 
     function checkMail(email) {
-        return /(\b\w+)@(([а-яA-z0-9][-а-яA-z0-9]+\.)+[а-яA-z]{2,4})/.test(email);
+        var regEmail = /^[-\w.]+@([A-zА-яЁё0-9][-A-zА-яЁё0-9]+\.)+[A-zА-яЁё]{2,4}$/;
+        return regEmail.test(email);
     }
 
     if (checkPhone(phone) && checkMail(email)) {
@@ -43,7 +44,34 @@ function checkedObject(object, query) {
    Поиск ведется по всем полям.
 */
 module.exports.find = function find(query) {
+    function editNumber(phone) {
+        var reg = /\d+/g;
+        var phone2 = '';
+        phone = phone.match(reg).join('');
+
+        for (var i = phone.length - 1; i >= 0; i--) {
+            phone2 += phone[i];
+            if ((i == phone.length - 3) || (i == phone.length - 4)) {
+                phone2 += '-';
+            } else if (i == phone.length - 7) {
+                phone2 += ' )';
+            } else if (i == phone.length - 10) {
+                phone2 += '( ';
+            }
+        }
+        if (phone.length == 10) {
+            phone2 += '7';
+        }
+        phone2 += '+';
+        return phone2.split('').reverse().join('');
+    }
+
     for (var i = 0; i < phoneBook.length; i++) {
+        if (!query) {
+            console.log(phoneBook[i].name + ', ' + editNumber(phoneBook[i].phone) +
+                ', ' + phoneBook[i].email);
+        }
+
         if (checkedObject(phoneBook[i], query)) {
             console.log(phoneBook[i].name + ', ' + phoneBook[i].phone + ', ' + phoneBook[i].email);
         }
@@ -72,10 +100,20 @@ module.exports.remove = function remove(query) {
 */
 module.exports.importFromCsv = function importFromCsv(filename) {
     var data = require('fs').readFileSync(filename, 'utf-8');
+    var strings = data.split('\n');
+    var counter = 0;
 
-    // Ваша чёрная магия:
-    // - Разбираете записи из `data`
-    // - Добавляете каждую запись в книгу
+    for (var i = 0; i < string.length; i++) {
+        var stringData = strings[i].replace('\r', '');
+        var stringPerson = stringData.split(';');
+        var nameData = stringPerson[0];
+        var phoneData = stringPerson[1];
+        var emailData = stringPerson[2];
+        if (module.exports.add(nameData, phoneData, emailData)) {
+            counter++;
+        }
+    }
+    console.log('Добавлено' + counter + 'контакта');
 };
 
 /*
